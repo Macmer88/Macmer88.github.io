3843cecb99deb1897f30dba6d2cd9d4a062da066
Maximiliano Merele <el.mazi.tgo@gmail.com>
2023-05-04 02:10:07 -0300

    "menu hamburguesa funcionando"

--- jscript/scripts.js
+++ jscript/scripts.js
@@ -1,3 +1,17 @@
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
+
+
+
 function mostrarContenido(){
     cartel = document.getElementById('formulario');
     if(cartel.style.display = 'none'){
